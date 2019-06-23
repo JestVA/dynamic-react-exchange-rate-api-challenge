3a12ec7479dfe19e157e3a956ccb3632ee2a5d49
Dorin <d.dumitrascuta@gmail.com>
2019-06-23 23:26:09 +0100

    write the date input component

--- src/component/user-input-date/user-input-date.js
+++ src/component/user-input-date/user-input-date.js
@@ -1,3 +1,14 @@
 import React from 'react'
-const UserInputDate = () => {}
+const UserInputDate = ({ onChange, date }) => {
+    return (
+        <div>
+            <p>Please select a date:</p>
+            <input 
+                type="date"
+                defaultValue={date}
+                onChange={e => onChange(e.target.value)}
+            />
+        </div>
+    )
+}
 export default UserInputDate
