0a5ee77fd245ce4e4981c90e34ba951a0f66452c
Dorin <d.dumitrascuta@gmail.com>
2019-06-23 22:58:10 +0100

    added logic for api call and props

--- src/exchange-rate-app.js
+++ src/exchange-rate-app.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import ExchangeRateResults from './component/exchange-rate-results'
 import UserInputCurrency from './component/user-input-currency'
 import UserInputDate from './component/user-input-date'
-
+import { initiateDate } from './util'
 
 const ExchangeRateApp = () => {
     // Let's define state: currency selection (base) & date input & the rates returned from the API poll
@@ -10,11 +10,31 @@ const ExchangeRateApp = () => {
     const defaultCurrency = "GBP"
     const [base, setBase] = useState(defaultCurrency)
     // Time Factor
-    const [date, setDate] = useState()
+    const [date, setDate] = useState(initiateDate())
     // API results
     const [rates, setRates] = useState(Object.create(null))
     
-    
+    // Making use of the useState hook to connect to the API and get the exchange rates 
+    useEffect(() => {
+        const apiURL = "https://api.exchangeratesapi.io/"
+        fetch(`${apiURL}${date}?base=${base}`)
+        .then(r => r.json())
+        .then(({ rates }) => setRates(rates))
+    }, [date, base])
+
+    // Props passing 
+    const userInputDateProps = {
+        date,
+        onChange: setDate
+    }
+
+    const userInputCurrencyProps = {
+        defaultCurrency,
+        onChange: setBase,
+        options: ["GBP", "EUR", "USD"],
+        size: 1
+    }
+
     
     return (
         <h1>building api poll...</h1>
