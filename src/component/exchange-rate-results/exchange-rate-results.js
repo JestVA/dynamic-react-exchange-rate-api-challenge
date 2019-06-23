f6ee4e908bb42d096371e38fcb27bfe6b1353540
Dorin <d.dumitrascuta@gmail.com>
2019-06-23 23:33:06 +0100

    And we got some rates :D

--- src/component/exchange-rate-results/exchange-rate-results.js
+++ src/component/exchange-rate-results/exchange-rate-results.js
@@ -1,4 +1,16 @@
 import React from 'react'
 
-const ExchangeRateResults = () => {}
+const ExchangeRateResults = ({ rateItems }) => {
+    return (
+        <div className="rate-items__wrapper">
+            <h2>Your results:</h2>
+            {Object.keys(rateItems).map(rateKey => (
+                <div className="rate-items-item" key={rateKey}>
+                    {`${rateKey}: ${rateItems[rateKey].toFixed(4)}`}
+                </div>
+            ))}
+
+        </div>
+    )
+}
 export default ExchangeRateResults
