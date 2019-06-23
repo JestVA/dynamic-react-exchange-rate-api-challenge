c2a5a549821286737d5ec29371807699d6e12d85
Dorin <d.dumitrascuta@gmail.com>
2019-06-23 22:42:22 +0100

    happy with my date function

---
+++ src/util/initiate-date.js
@@ -0,0 +1,7 @@
+export const initiateDate = () => {
+    const tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    const localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+    const currentDate = localISOTime.match(/\d{4}(-\d\d){2}/g)[0]
+    return currentDate
+}
+
